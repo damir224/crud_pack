500412d1ffa91158482f8fea9f97c4c73ec64330
damir224 <damir224@bk.ru>
2021-04-14 19:42:19 +0500

    optiStable

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -15,50 +15,56 @@ import { likeCardSagaAC } from '../store/cards/actions';
 import Like from './Like';
 import useStyles from '../helpers/styles/useStylesSimpleCard';
 
-export default function SimpleCard({
-  card: {
-    id, title, content, liked, total_likes,
-  },
-}) {
-  const classes = useStyles();
-  const { token, role } = useSelector(({ userReducers }) => userReducers.user);
-  const dispatch = useDispatch();
+const SimpleCard = React.memo(
+  ({ card: { id, title, content, liked, total_likes } }) => {
+    const classes = useStyles();
+    const { token, role } = useSelector(
+      ({ userReducers }) => userReducers.user,
+    );
+    const dispatch = useDispatch();
 
-  return (
-    <Card
-      className={classes.root}
-      style={{
-        border: 10,
-        borderStyle: 'solid',
-        borderRadius: 1,
-        borderWidth: 1,
-      }}
-    >
-      <CardContent>
-        <Typography variant='h6' component='h5'>
-          {title}
-        </Typography>
-        <Typography variant='body2' component='p' style={{ marginTop: '1em' }}>
-          {content}
-        </Typography>
-      </CardContent>
-      <CardActions className={classes.button}>
-        {role === 'admin' ? (
-          <Link to={`/info/${id}`}>
-            <Button size='small'>
-              <EditIcon />
-            </Button>
-          </Link>
-        ) : null}
-        <Button
-          size='small'
-          onClick={() => {
-            dispatch(likeCardSagaAC({ id, token }));
-          }}
-        >
-          <Like like={{ liked, total_likes }} />
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
+    return (
+      <Card
+        className={classes.root}
+        style={{
+          border: 10,
+          borderStyle: 'solid',
+          borderRadius: 1,
+          borderWidth: 1,
+        }}
+      >
+        <CardContent>
+          <Typography variant='h6' component='h5'>
+            {title}
+          </Typography>
+          <Typography
+            variant='body2'
+            component='p'
+            style={{ marginTop: '1em' }}
+          >
+            {content}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.button}>
+          {role === 'admin' ? (
+            <Link to={`/info/${id}`}>
+              <Button size='small'>
+                <EditIcon />
+              </Button>
+            </Link>
+          ) : null}
+          <Button
+            size='small'
+            onClick={() => {
+              dispatch(likeCardSagaAC({ id, token }));
+            }}
+          >
+            <Like like={{ liked, total_likes }} />
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  },
+);
+
+export default SimpleCard;
